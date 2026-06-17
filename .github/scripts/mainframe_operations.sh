e134b000dd521e9637ea5d719c451753f7713cce
iamsharon2621 <callsharonlee-2047@yahoo.com.hk>
2026-06-17 14:58:57 +0100

    extra: debug

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -11,8 +11,8 @@ java -version
 
 # Set ZOWE_USERNAME
 # ZOWE_USERNAME="Z80922" # Replace with the actual username
-echo "${X_USERNAME}"
 echo "${LOWERCASE_USERNAME}"
+echo "${ZOWE_USER}"
 
 # Change to the cobolcheck directory
 cd cobol-check
@@ -48,10 +48,10 @@ run_cobolcheck() {
   # Check if CC##99.CBL was created, regardless of cobolcheck exit status
   if [ -f "${GENERATED_CBL}" ]; then
     # Copy to the MVS dataset
-    if cp ${GENERATED_CBL} "//'${X_USERNAME}.CBL($program)'"; then
-      echo "Copied CC##99.CBL to ${X_USERNAME}.CBL($program)"
+    if cp ${GENERATED_CBL} "//'${ZOWE_USER}.CBL($program)'"; then
+      echo "Copied CC##99.CBL to ${ZOWE_USER}.CBL($program)"
     else
-      echo "Failed to copy CC##99.CBL to ${X_USERNAME}.CBL($program)"
+      echo "Failed to copy CC##99.CBL to ${ZOWE_USER}.CBL($program)"
       exit 1
     fi
   else
@@ -61,10 +61,10 @@ run_cobolcheck() {
 
   # Copy the JCL file if it exists
   if [ -f "${PGM_JCL}" ]; then
-    if cp ${PGM_JCL} "//'${X_USERNAME}.JCL($program)'"; then
-      echo "Copied ${PGM_JCL} to ${X_USERNAME}.JCL($program)"
+    if cp ${PGM_JCL} "//'${ZOWE_USER}.JCL($program)'"; then
+      echo "Copied ${PGM_JCL} to ${ZOWE_USER}.JCL($program)"
     else
-      echo "Failed to copy ${PGM_JCL} to ${X_USERNAME}.JCL($program)"
+      echo "Failed to copy ${PGM_JCL} to ${ZOWE_USER}.JCL($program)"
       exit 1
     fi
   else
