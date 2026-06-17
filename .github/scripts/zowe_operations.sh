29abe3f6594a0d75294fdf2d479d8aca7ad29386
iamsharon2621 <callsharonlee-2047@yahoo.com.hk>
2026-06-17 07:18:18 +0100

    extra: move LOWERCASE_USERNAME from zowe_operations to workflow

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -2,20 +2,20 @@
 # zowe_operations.sh
 
 # Convert username to lowercase
-LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+#LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+if ! zowe zos-files list uss-files "/z/${LOWERCASE_USERNAME}/cobolcheck" &>/dev/null; then
   echo "Directory does not exist. Creating it..."
-  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+  zowe zos-files create uss-directory /z/${LOWERCASE_USERNAME}/cobolcheck
 else
   echo "Directory already exists."
 fi
 
 # Upload files
-  zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive \
+  zowe zos-files upload dir-to-uss "./cobol-check" "/z/${LOWERCASE_USERNAME}/cobolcheck" --recursive \
         --binary-files "cobol-check-0.2.19.jar"
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "/z/${LOWERCASE_USERNAME}/cobolcheck"
